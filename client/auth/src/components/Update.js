ea28f8b431717b215d6f91ec061d713e4b76e1a2
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2022-03-16 17:33:41 +0100

    get the data by rendering with UseEffect and set the initial values correct

--- client/auth/src/components/Update.js
+++ client/auth/src/components/Update.js
@@ -1,31 +1,45 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Axios from "axios";
 import {Link} from "react-router-dom";
 
 
 function Update() {
     const [actualAccount,setActualAccount] = useState("Alfresco");
+    const [userData,setUserData] = useState([{name:"(Lädt Namen)", employer:"(Lädt)", age:"(Lädt Alter)", password:""}]);
 
-    const getUser = () => {
+    useEffect(() => {
         Axios.post('http://localhost:3001/getUser', {
           actualAccountKey: actualAccount,
         }).then((response) => {
-        setUserData(response.data)
+        //setUserData(response.data)
         console.log("Front End: getSpecificUser: response Data: "+ response.data) 
-    
+        setUserData(response.data)
+        setName(response.data[0].name)
+        setEmployer(response.data[0].employer)
+        setAge(response.data[0].age)
+        setPassword(response.data[0].password)
         });
     
-      }
-    
-  const [userData,setUserData] = useState([]);
-  console.log(userData)
-  const a = userData.age;
-  console.log(a)
-  const [name,setName] = useState(userData.name);
-  const [employer,setEmployer] = useState(userData.employer);
-  const [age,setAge] = useState(userData.age);
-  const [password,setPassword] = useState(userData.password);
+      },[actualAccount])
   
+  
+  if (userData.length > 0){
+    console.log(userData[0].name)
+  } else{
+    console.log("Noch kein Array vorhanden")
+  }
+
+  const [name,setName] = useState(userData[0].name);
+  console.log("Die Länge des Arrays " + userData.length)
+  console.log("Der State name " + name)
+  const [employer,setEmployer] = useState(userData[0].employer);
+  const [age,setAge] = useState(userData[0].age);
+  const [password,setPassword] = useState(userData[0].password);
+  
+  console.log("hier ist der name " + name + " hinterlegt")
+  console.log("hier ist der employer " + employer + " hinterlegt")
+  console.log("hier ist der ageg " + age + " hinterlegt")
+  console.log("hier ist der pw " + password + " hinterlegt")
 
   console.log("Der aktuelle Account ist:" + actualAccount)
   
@@ -47,28 +61,47 @@ function handleSelectEmployer(event) {
             <option value="Ute">Ute</option>
         </select>
     <h1>Update your Site</h1>
+    <h1>
+    {userData.map((value, key) => {
+        return(
+        <div className="Card_Wrapper">  
+        <div className="Reservation_Card">
+        <div className="ListEntry" key={key}>Hallo: {value.name} </div>
+        {/*<button className="FormButton2" onClick={() => deleteReservation(value.id)}>Reservierung stornieren</button>*/}
+        </div>
+        </div>
+        
+        )
+      })}    
+    </h1>
  
     <div className="App">
       <div className="Form">
-      <h3>Name</h3>
+      {userData.map((value, key) => {
+        return(
+        <h3 className="ListEntry" key={key}>Name: {value.name} </h3>
+        )
+      })}    
+      Ihr Neuer Username:
        <input className="Inputfield" type="text" value={name} onChange={(e) => {setName(e.target.value)}}></input>
         <h3>Arbeitgeber</h3>
         <select value={employer} onChange={handleSelectEmployer}>
             <option value="BVA"> BVA</option>
             <option value="Accenture">Accenture</option>
             <option value="Pascha">Pascha</option>
+            <option value="(Lädt)">Lädt</option>
         </select>
        <h3>Alter</h3>
        <input className="Inputfield" type="text" value={age} onChange={(e) => {setAge(e.target.value)}}></input>
        <h3>Password</h3>
-       <input className="Inputfield" type="password" value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
-       <button className="FormButton" onClick={() => getUser()}>Bestimmten User anzeigen.</button>
+       <input className="Inputfield" type="text" value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
+       {/*<button className="FormButton" onClick={() => getUser()}>Bestimmten User anzeigen.</button>*/}
 
        {userData.map((value, key) => {
         return(
         <div className="Card_Wrapper">  
         <div className="Reservation_Card">
-        <div className="ListEntry" key={key}>Name: {value.name}</div>
+        <div className="ListEntry" key={value.name}>Name: {value.name}</div>
         <div className="ListEntry">Arbeitgeber: {value.employer}</div>
         <div className="ListEntry">Alter: {value.age}</div>
         <div className="ListEntry">PW: {value.password}</div>
